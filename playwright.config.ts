e18c9e13a88fa7181c8447fc63b0a6a1c2242ff1
Marcin P <marcin.pawlowski@hicron.com>
2025-04-08 14:07:19 +0200

    config: add .env configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,5 @@
+import { Configuration } from '@_e2e/config/variables.config';
 import { defineConfig, devices } from '@playwright/test';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-
-/**
- * See https://playwright.dev/docs/test-parameterize#env-files
- */
-dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -13,13 +7,15 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
 export default defineConfig({
   testDir: './e2e/tests/',
   outputDir: 'e2e/output/test-results',
+  globalSetup: './e2e/config/global-setup.ts',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  // forbidOnly: !!process.env.CI,
+  // retries: process.env.CI ? 1 : 0,
+  // workers: process.env.CI ? 1 : undefined,
   reporter: [['html', { open: 'never', outputFolder: 'e2e/output/test-reports' }]],
   use: {
     // baseURL: process.env.CI ? process.env.PLAYWRIGHT_BASE_URL : 'http://localhost:3000',
+    baseURL: Configuration.baseURL,
     trace: 'retain-on-failure',
     deviceScaleFactor: 1,
   },
